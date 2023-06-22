85a02a0bbee71ddd4354401e698ef6c87cbe376e
Matthew Russell <matthew.g.russell@gmail.com>
2023-06-22 16:40:21 +0000

    Fix screenlayout

--- stow/screenlayout/.screenlayout/ugc15x24r53.sh
+++ stow/screenlayout/.screenlayout/ugc15x24r53.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-xrandr \
-  --output DP-0 --primary --mode 1680x1050 --pos 0x795 --rotate normal \
-  --output DP-1 --off \
-  --output DP-2 --off \
-  --output DP-3 --off \
-  --output DP-4 --mode 1920x1200 --pos 3600x0 --rotate left \
-  --output DP-5 --off \
-  --output DP-6 --mode 1920x1200 --pos 1680x720 --rotate normal \
-  --output DP-7 --off
+xrandr --output DP-0 --mode 1920x1200 --pos 4480x0 --rotate left --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --primary --mode 2560x1080 --pos 1920x840 --rotate normal --output DP-5 --off --output DP-6 --mode 1920x1200 --pos 0x720 --rotate normal --output DP-7 --off
