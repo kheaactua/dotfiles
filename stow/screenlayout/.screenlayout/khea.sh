4c4b641ef015f625d928e3075ea1dd38f4a1de0e
Matthew Russell <matthew.g.russell@gmail.com>
2024-03-10 11:55:46 -0400

    pdf black/white fuction

--- stow/screenlayout/.screenlayout/khea.sh
+++ stow/screenlayout/.screenlayout/khea.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --mode 1920x1200 --pos 0x720 --rotate normal --output DisplayPort-1 --mode 2560x1080 --pos 1920x840 --rotate normal --output DisplayPort-2 --mode 1920x1200 --pos 4480x0 --rotate left --output HDMI-A-0 --off
+xrandr --output DisplayPort-0 --mode 1920x1200 --pos 0x720 --rotate normal --output DisplayPort-1 --primary --mode 2560x1080 --pos 1920x840 --rotate normal --output DisplayPort-2 --mode 1920x1200 --pos 4480x0 --rotate left --output HDMI-A-0 --off
