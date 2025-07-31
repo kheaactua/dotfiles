462429575427e69dc9093862b216eda55ae9c645
Matthew Russell <matthew.g.russell@gmail.com>
2025-07-31 11:42:33 -0400

    Using daisy chaining

--- stow/screenlayout/.screenlayout/khea.sh
+++ stow/screenlayout/.screenlayout/khea.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --mode 1920x1200 --pos 0x720 --rotate normal --output DisplayPort-1 --primary --mode 2560x1080 --pos 1920x840 --rotate normal --output DisplayPort-2 --mode 1920x1200 --pos 4480x0 --rotate left --output HDMI-A-0 --off
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --off --output DisplayPort-3 --mode 2560x1080 --pos 1920x780 --rotate normal --output DisplayPort-4 --off --output DisplayPort-5 --mode 1920x1200 --pos 0x720 --rotate normal --output DisplayPort-6 --mode 1920x1200 --pos 4480x0 --rotate left
