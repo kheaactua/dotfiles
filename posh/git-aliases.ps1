4c01adf48a4ad9b77a9d18bc952e8bddfecdff3a
Matthew Russell <mruss100@ford.com>
2025-08-14 15:54:31 -0400

    Adding some pwsh scripts

--- posh/git-aliases.ps1
+++ posh/git-aliases.ps1
@@ -5,12 +5,11 @@ Write-Host "Loading git-aliases" -ForegroundColor Yellow
 # originally I had to do this, but maybe posh-git takes care of it?  Uncertain,
 # leaving this for now.  It's also super slow.
 # Install-Module git-aliases -Scope CurrentUser -AllowClobber
-Import-Module posh-git
+# Import-Module git-aliases
+# Import-Module posh-git
 
 # Override
 function gst { git status -uno -sb $args }
 
-# BFG https://rtyley.github.io/bfg-repo-cleaner/
-function bfg { java.exe -jar c:\GitRepo\bfg-1.13.0.jar $args }
 
 # vim: ts=4 sw=4 sts=0 noexpandtab ff=dos ft=ps1 :
