eb6bfc4d93cd4e5444de430c85f5e55c801b6221
Matt <mruss100@ford.com>
2025-09-16 16:32:52 -0400

    Set OSC 52 in nvim

--- stow/neovim/.config/nvim/lua/init.lua
+++ stow/neovim/.config/nvim/lua/init.lua
@@ -1,2 +1,6 @@
-require("config.lazy")
 -- Configure colorscheme and cmp via plugins/lazy.nvim
+require("config.lazy")
+
+-- Set up the clipboard to use osc52, which allows copying to the system clipboard
+-- This is useful when using neovim inside a docker container or a remote server.
+vim.g.clipboard = 'osc52'
