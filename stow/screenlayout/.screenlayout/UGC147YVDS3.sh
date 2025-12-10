ec74b6937a83ccf7ac4c941e2cadaaeb067d2405
Matthew Russell <mruss100@ford.com>
2025-12-10 08:16:57 -0500

    Change kitty theme from Cobalt2 to Ayu Mirage

---
+++ stow/screenlayout/.screenlayout/UGC147YVDS3.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1366x768 --pos 597x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-1-1 --mode 2560x1080 --pos 0x838 --rotate normal --output DP-1-2 --primary --mode 3440x1440 --pos 2560x478 --rotate normal --output DP-1-3 --off
