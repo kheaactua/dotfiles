c448ff687bab0387c1152f051bd051116f96c4d9
Matthew Russell <matthew.g.russell@gmail.com>
2025-12-17 08:27:46 -0500

    Look around for the module file

--- load_env_modules.sh
+++ load_env_modules.sh
@@ -1,34 +1,25 @@
 #!/bin/zsh
 
 # This file is symlinked here after env modules is installed from source
-declare module_profile_file="/etc/profile.d/modules.sh"
-if [[ -e "${module_profile_file}" ]]; then
-  declare -f module > /dev/null || . "${module_profile_file}"
+# I suspect I messed up the manual install so now I guess at which file to source
+declare -a module_candidates=(
+  /etc/profile.d/env-modules.sh
+  /etc/profile.d/modules.sh
+  /usr/share/Modules/init/zsh
+)
+for module_file in "${module_candidates[@]}"; do
+  if [[ -e "${module_file}" ]]; then
+    declare -f module > /dev/null || . "${module_file}"
+    break
+  fi
+done
 
-	# modules_enabled=1;
-
-	# Environmental Modules
-	# The module command is now installed in /etc/profile.d/env-modules.sh
-
-	# case "$0" in
-	# -sh|sh|*/sh)        modules_shell=sh   ;;
-	# -ksh|ksh|*/ksh)     modules_shell=ksh  ;;
-	# -zsh|zsh|*/zsh)     modules_shell=zsh  ;;
-	# -bash|bash|*/bash)  modules_shell=bash ;;
-	# esac
-
-	# export MODULEPATH=/usr/share/modules/modulefiles
-
-	# # #module() { eval `/usr/Modules/$MODULE_VERSION/bin/modulecmd $modules_shell $*`; }
-	# modulecmd=$(which modulecmd)
-	# module() { eval `${modulecmd} $modules_shell $*`; }
-
-
+if ! declare -f module > /dev/null; then
+  echo "Environment modules not installed"
+else
   if [[ -e "${HOME}/.modulefiles" ]]; then
     module use "${HOME}/.modulefiles"
   fi
-else
-  echo "Environment modules ($module_profile_file) not installed"
 fi
 
 # vim: ts=2 sw=2 et :
