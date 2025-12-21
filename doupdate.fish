eda2f35f7f8fbe33f562c29762ab1780d1ad8e8e
Matthew Russell <matthew.g.russell@gmail.com>
2025-12-21 13:10:35 -0500

    Fish verion of doupdate, removed vim-tmux-navigator from tmux

---
+++ doupdate.fish
@@ -0,0 +1,35 @@
+#!/usr/bin/env fish
+
+function abort_update --on-signal SIGHUP SIGINT SIGQUIT SIGABRT
+    echo "Aborting dotfiles update"
+end
+
+function update_dotfiles
+    set -l cwd (dirname (realpath (status --current-filename)))
+    cd $cwd; or echo "Could not change to $cwd"
+    
+    set -l check_file (set -q DOTFILES_DIR; and echo $DOTFILES_DIR; or dirname (realpath (status --current-filename)))/.last_check
+
+    set -l now (date +%s)
+    if not test -e $check_file
+        echo 0 > $check_file
+    end
+    
+    set -l last_update (cat $check_file)
+    if test -z "$last_update"
+        set last_update 0
+    end
+
+    # Three days
+    set -l s (math $last_update + 259200)
+    if test $now -gt $s
+        echo "Checking for update to dotfiles...."
+        set -l dotfiles_path (set -q DOTFILES_DIR; and echo $DOTFILES_DIR; or echo ~/dotfiles)
+        env GIT_DIR=$dotfiles_path/.git GIT_WORK_TREE=$dotfiles_path/ git pull
+        echo $now > $check_file
+    else
+        # echo "Not checking for update to dotfiles...."
+    end
+end
+
+update_dotfiles
--- stow/fish/.config/fish/conf.d/00-dotfiles.fish
+++ stow/fish/.config/fish/conf.d/00-dotfiles.fish
@@ -5,3 +5,8 @@ end
 if not set -q DOTFILES_SECRET_DIR
     set -gx DOTFILES_SECRET_DIR "$DOTFILES_DIR/dotfiles-secret"
 end
+
+# Update the dotfiles repo to make sure we have all changes:
+if test -e "$DOTFILES_DIR/doupdate.fish"
+    source "$DOTFILES_DIR/doupdate.fish" > /dev/null 2>&1
+end
