112638e451d3843b00bf29cf9f1f9b812a2c19b6
Matthew Russell <matthew.g.russell@gmail.com>
2026-02-27 14:11:43 -0500

    Trying to get the clipboard to work in all situations

--- stow/neovim/.config/nvim/lua/init.lua
+++ stow/neovim/.config/nvim/lua/init.lua
@@ -1,6 +1,24 @@
 -- Configure colorscheme and cmp via plugins/lazy.nvim
 require("config.lazy")
 
--- Set up the clipboard to use osc52, which allows copying to the system clipboard
--- This is useful when using neovim inside a docker container or a remote server.
-vim.g.clipboard = 'osc52'
+-- Set up clipboard to work optimally in all situations:
+-- - Local with system clipboard tools: use xclip/wl-clipboard (fastest, most reliable)
+-- - Remote SSH or tmux: OSC 52 works through terminal escape sequences
+-- - Auto-detects and uses the best available method
+if os.getenv("SSH_CONNECTION") or os.getenv("TMUX") then
+  -- Use OSC 52 for remote/tmux sessions
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
+else
+  -- Use system clipboard tools for local sessions (faster, more reliable)
+  vim.opt.clipboard = 'unnamedplus'
+end
